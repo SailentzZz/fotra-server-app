b244163fea604ec22cbe9c19cddf35217fdc533c
Александр Политов <43606198+AlexandrPolitov@users.noreply.github.com>
2019-10-20 21:55:19 +0300

    Add Yandex Translate

---
+++ src/main/java/start/SipProjectApplication.java
@@ -0,0 +1,11 @@
+package start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SipProjectApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SipProjectApplication.class, args);
+	}
+}
