6b5e028428e2e44be4127311dcde408b4301a552
SailentzZz <mishatravin99@gmail.com>
2019-10-26 15:42:23 +0300

    Test controller and that work!

--- src/main/java/controllers/Translation.java
+++ src/main/java/controllers/Translation.java
@@ -1,37 +1,30 @@
 package controllers;
 
-import java.io.UnsupportedEncodingException;
+import org.springframework.stereotype.Controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import repos.UserForumRepository;
-import translation.GoogleTranslate;
-import translation.Translate;
-import translation.YandexTranslate;
-import translation.models.TranslResult;
-
-@RestController
-@RequestMapping(value = "/api/translate")
+@Controller
+//@RequestMapping(value = "/api/translate")
 public class Translation {
-    private final Translate yandexTranslate;
-    private final Translate googleTranslate;
-
-    @Autowired
-    UserForumRepository userForum;
-    
-    @Autowired
-    public Translation(YandexTranslate yandexTranslate, GoogleTranslate googleTranslate) {
-        this.yandexTranslate = yandexTranslate;
-        this.googleTranslate = googleTranslate;
-    }
-
-    @GetMapping("/yandex/{lang}/{text}")
-    public TranslResult translate(@PathVariable("lang") String lang,
-                                  @PathVariable("text") String text) throws UnsupportedEncodingException {
-        return yandexTranslate.getTranslate(lang, text);
-    }
+//    private final Translate yandexTranslate;
+//    private final Translate googleTranslate;
+//
+////    @Autowired
+////    DAORepository<UserForum, Long> userForum;
+//    
+//    @Autowired
+//    public Translation(YandexTranslate yandexTranslate, GoogleTranslate googleTranslate) {
+//        this.yandexTranslate = yandexTranslate;
+//        this.googleTranslate = googleTranslate;
+//    }
+//
+//    @RequestMapping("/hello")
+//    public String hello() {
+//        return "Hello World!";
+//    }
+//    
+//    @GetMapping("/yandex/{lang}/{text}")
+//    public TranslResult translate(@PathVariable("lang") String lang,
+//                                  @PathVariable("text") String text) throws UnsupportedEncodingException {
+//        return yandexTranslate.getTranslate(lang, text);
+//    }
 }
